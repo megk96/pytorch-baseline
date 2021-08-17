d9a9ce054d410a4c2d0f647589e9790f336ebf85
meghana <sonamegz@gmail.com>
2021-08-17 18:10:28 +0100

    add label embeddings

--- models/cnn1.py
+++ models/cnn1.py
@@ -4,9 +4,12 @@ import torch.nn.functional as F
 
 
 class CNN1(nn.Module):
-    def __init__(self, num_classes):
+    def __init__(self, num_classes, label=False):
         super().__init__()
-        self.conv1 = nn.Conv2d(3, 6, 5)
+        if label:
+            self.conv1 = nn.Conv2d(1, 6, 5)
+        else:
+            self.conv1 = nn.Conv2d(3, 6, 5)
         self.pool = nn.MaxPool2d(2, 2)
         self.conv2 = nn.Conv2d(6, 16, 5)
         self.fc1 = nn.Linear(16 * 5 * 5, 120)
